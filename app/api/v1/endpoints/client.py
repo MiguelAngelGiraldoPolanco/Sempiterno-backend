96e72cd38ad64acdf1cdb87819a89de41ecb49c8
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-11 14:08:09 +0200

    style: modify router and function visual

--- app/api/v1/endpoints/client.py
+++ app/api/v1/endpoints/client.py
@@ -12,21 +12,35 @@ from sqlmodel import Session
 router = APIRouter(prefix="/clients", tags=["Clients"])
 
 
-@router.get("/", response_model=list[client.ClientRead])
-def read_client(db: Session = Depends(database.get_session)):
+@router.get(
+    "/",
+    response_model=list[client.ClientRead],
+)
+def read_client(
+    db: Session = Depends(database.get_session),
+):
     return client_service.obtener_todos_los_clients(db)
 
 
-@router.get("/reporte/fechas", response_model=list[client.ClientRead])
+@router.get(
+    "/reporte/fechas",
+    response_model=list[client.ClientRead],
+)
 def reporte_mensual(
-    inicio: datetime, fin: datetime, db: Session = Depends(database.get_session)
+    inicio: datetime,
+    fin: datetime,
+    db: Session = Depends(database.get_session),
 ):
     return client_service.obtener_clients_por_fecha(db, inicio, fin)
 
 
-@router.get("/email/{email_cliente}", response_model=client.ClientRead)
+@router.get(
+    "/email/{email_cliente}",
+    response_model=client.ClientRead,
+)
 def read_client_email(
-    email_cliente: EmailStr, db: Session = Depends(database.get_session)
+    email_cliente: EmailStr,
+    db: Session = Depends(database.get_session),
 ):
     client = client_service.obtener_clients_por_email(db, email_cliente)
     if not client:
@@ -37,8 +51,13 @@ def read_client_email(
     return client
 
 
-@router.post("/", response_model=client.ClientRead, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/",
+    response_model=client.ClientRead,
+    status_code=status.HTTP_201_CREATED,
+)
 def crear_cliente(
-    client_in: client.ClientCreate, db: Session = Depends(database.get_session)
+    client_in: client.ClientCreate,
+    db: Session = Depends(database.get_session),
 ):
     return client_service.crear_client(db, client_in)
--- app/services/client_service.py
+++ app/services/client_service.py
@@ -8,7 +8,10 @@ from pydantic import EmailStr
 from sqlmodel import Session, select
 
 
-def crear_client(db: Session, client_data: ClientCreate) -> Client:
+def crear_client(
+    db: Session,
+    client_data: ClientCreate,
+) -> Client:
     # Evaluamos primero si el email existe para que no pueda acceder a muchos desceuntos con el mismo email
     if obtener_clients_por_email(db, client_data.email):
         raise HTTPException(
@@ -31,12 +34,16 @@ def crear_client(db: Session, client_data: ClientCreate) -> Client:
     return nuevo_cliente
 
 
-def obtener_todos_los_clients(db: Session) -> Sequence[Client]:
+def obtener_todos_los_clients(
+    db: Session,
+) -> Sequence[Client]:
     return db.exec(select(Client)).all()
 
 
 def obtener_clients_por_fecha(
-    db: Session, init_date: datetime, last_date: datetime
+    db: Session,
+    init_date: datetime,
+    last_date: datetime,
 ) -> Sequence[Client]:
 
     if init_date > last_date:
@@ -52,13 +59,18 @@ def obtener_clients_por_fecha(
     return db.exec(sentencia).all()
 
 
-def obtener_clients_por_email(db: Session, email: EmailStr) -> Client | None:
+def obtener_clients_por_email(
+    db: Session,
+    email: EmailStr,
+) -> Client | None:
     sentencia = select(Client).where(Client.email == email)
 
     return db.exec(sentencia).first()
 
 
-def asegurar_utc(dt: datetime) -> datetime:
+def asegurar_utc(
+    dt: datetime,
+) -> datetime:
     if dt.tzinfo is None:
         return dt.replace(tzinfo=timezone.utc)
     return dt.astimezone(timezone.utc)
