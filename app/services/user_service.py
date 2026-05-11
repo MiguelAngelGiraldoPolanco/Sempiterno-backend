96963ed7b9b9aee3402e38a824c4e21060190286
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-11 13:43:13 +0200

    refactor: change User class to dict in modificar_usuario

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,17 +1,19 @@
 from datetime import datetime, timezone
-from typing import Optional
 
 from app.models.user import User
 from app.schemas.user import UserCreate
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from pydantic import EmailStr
 from sqlmodel import Session, select
 
 
-def create_user(db: Session, user_data: UserCreate) -> Optional[User]:
+def create_user(db: Session, user_data: UserCreate) -> User:
     # if para asegurar que el cliente no exista en la base de datos
     if obtener_usuario_por_email(db, user_data.email):
-        raise HTTPException(status_code=400, detail="EL usuario ya existe")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="El cliente ya existe",
+        )
     nuevo_user = User(**user_data.model_dump())
     db.add(nuevo_user)
     try:
@@ -19,7 +21,8 @@ def create_user(db: Session, user_data: UserCreate) -> Optional[User]:
     except Exception:
         db.rollback()
         raise HTTPException(
-            status_code=400, detail="Error de integridad, intente de nuevo"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Error de integridad, intente de nuevo",
         )
 
     db.refresh(nuevo_user)
@@ -27,11 +30,11 @@ def create_user(db: Session, user_data: UserCreate) -> Optional[User]:
     return nuevo_user
 
 
-def obtener_usuario_por_id(db: Session, user_id: int) -> Optional[User]:
+def obtener_usuario_por_id(db: Session, user_id: int) -> User | None:
     return db.get(User, user_id)
 
 
-def obtener_usuario_por_email(db: Session, user_email: EmailStr) -> Optional[User]:
+def obtener_usuario_por_email(db: Session, user_email: EmailStr) -> User | None:
     sentencia = select(User).where(User.email == user_email)
 
     return db.exec(sentencia).first()
@@ -41,7 +44,7 @@ def modificar_usuario(db: Session, user_id: int, user_data: User) -> User:
     user_db = db.get(User, user_id)
     if not user_db:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Usuario no encontrado",
         )
 
@@ -57,7 +60,7 @@ def modificar_usuario(db: Session, user_id: int, user_data: User) -> User:
     except Exception:
         db.rollback()
         raise HTTPException(
-            status_code=400,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="No se pudo actualizar: el email ya está en uso o hubo un error interno",
         )
 
@@ -65,7 +68,10 @@ def modificar_usuario(db: Session, user_id: int, user_data: User) -> User:
 def eliminar_usuario(db: Session, user_id: int) -> dict:
     user_db = db.get(User, user_id)
     if not user_db:
-        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Usuario no encontrado",
+        )
     db.delete(user_db)
     db.commit()
     return {"ok": True, "message": "Usuario eliminado"}
